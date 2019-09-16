6c6cf2aa7ed8cfc043b9675922ce4382322e47d1
taher elsheikh <taher@gwmail.gwu.edu>
2019-09-15 18:32:22 -0700

    Updates

---
+++ precache-manifest.718d26c80dadd03a3b7e897b50b9428d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24675e3eb7ad2a3b8fba5592182ace85",
+    "url": "/mysideprojectofficalwebsite/index.html"
+  },
+  {
+    "revision": "8e08bb055d5d77ef35c0",
+    "url": "/mysideprojectofficalwebsite/static/css/main.5a1bdb77.chunk.css"
+  },
+  {
+    "revision": "6f89dbfa557f19cffdb0",
+    "url": "/mysideprojectofficalwebsite/static/js/2.e6b56aa1.chunk.js"
+  },
+  {
+    "revision": "8e08bb055d5d77ef35c0",
+    "url": "/mysideprojectofficalwebsite/static/js/main.fbcd2635.chunk.js"
+  },
+  {
+    "revision": "20ed2da6528aa6594e41",
+    "url": "/mysideprojectofficalwebsite/static/js/runtime~main.4ce87e4e.js"
+  },
+  {
+    "revision": "a180f2e94dd895826b40bd5a183c4720",
+    "url": "/mysideprojectofficalwebsite/static/media/MySideProjectLogo.a180f2e9.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mysideprojectofficalwebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
